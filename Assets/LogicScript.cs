8ea0f7c2e96b0dcde240c2749f8adb4996dd73b5
Ahsan Faisal <61789531+AUAboi@users.noreply.github.com>
2024-06-09 19:26:44 +0500

    added scripts

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase score")]    
+    public void AddScore(int score = 1)
+    {
+        playerScore += score;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
